34db13ace755aaddad8f55a4217ae85a3b80d4a2
ezdall <67018806+ezdall@users.noreply.github.com>
2024-05-11 22:54:38 +0800

    refactor server.js, separate app(express) & server
    - separate code for testing
    - put express-related, middlewares to app.js
    - put mongoose, http.createServer to server.js

---
+++ app.js
@@ -0,0 +1,46 @@
+require('express-async-errors');
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const morgan = require('morgan');
+// const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+
+const { corsOptions } = require('./helpers/cors-opts');
+const { errorHandler } = require('./helpers/error-handler');
+//
+const { userRouter } = require('./routes/user.route');
+
+const app = express();
+
+app.use(cors(corsOptions));
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // false
+app.use(cookieParser());
+// app.use(helmet());
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.use('/', [userRouter]);
+
+// catch all
+app.all('*', (req, res, next) => {
+  const error = new Error(`${req.ip} tried to access ${req.originalUrl}`);
+
+  return next(error);
+});
+
+// error handle
+app.use(errorHandler);
+
+module.exports = app;
+
+/**
+ * /signin --> POST = success/fail
+ * /register --> POST = user
+ * /profile/:userId --> GET = user
+ * /image --> PUT --> user
+ */
--- server.js
+++ server.js
@@ -1,49 +1,15 @@
-require('express-async-errors');
-require('dotenv').config();
-
-const express = require('express');
+const http = require('http');
 const mongoose = require('mongoose');
-const cors = require('cors');
-const path = require('path');
-const morgan = require('morgan');
-// const helmet = require('helmet');
-const cookieParser = require('cookie-parser');
 
+const app = require('./app');
 const { connectMDB } = require('./db');
-const { corsOptions } = require('./helpers/cors-opts');
-const { errorHandler } = require('./helpers/error-handler');
-//
-const { userRouter } = require('./routes/user.route');
 
-// connect
 connectMDB().catch(err => console.error('connect-mongodb Error', err.stack));
-
-const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors(corsOptions));
-app.use(morgan('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // false
-app.use(cookieParser());
-// app.use(helmet());
+const server = http.createServer(app);
 
-app.use('/', express.static(path.join(__dirname, 'public')));
-
-app.use('/', [userRouter]);
-
-// catch all
-app.all('*', (req, res, next) => {
-  const error = new Error(`${req.ip} tried to access ${req.originalUrl}`);
-
-  return next(error);
-});
-
-// error handle
-app.use(errorHandler);
-
-//
 mongoose.connection.once('open', () => {
   const isProd = process.env.NODE_ENV === 'production';
 
@@ -52,7 +18,7 @@ mongoose.connection.once('open', () => {
     ? 'detect-face-srv.onrender.com'
     : `localhost:${PORT}`;
 
-  app.listen(PORT, err => {
+  server.listen(PORT, err => {
     if (err) throw err;
     console.log(`DetectFace-Srv -${nodeEnv}- running at ${hostNamePort}`);
   });
@@ -62,10 +28,3 @@ mongoose.connection.once('open', () => {
 mongoose.connection.on('error', err => {
   console.error('error @mongoose-conn-error ---', err);
 });
-
-/**
- * /signin --> POST = success/fail
- * /register --> POST = user
- * /profile/:userId --> GET = user
- * /image --> PUT --> user
- */
